7ce10a1fbdab8980b54d36e0e471a0fe2f90cd2b
Chrissivert <chrisssy@stud.ntnu.no>
2023-07-21 01:10:50 +0200

    added bulletSpeed powerup

--- Assets/C# Scripts/DoubleFireRate.cs
+++ Assets/C# Scripts/DoubleFireRate.cs
@@ -1,29 +1,26 @@
 using System;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class IncreaseFireRate : MonoBehaviour
 {
-    public float doublefirerateduration = 20f;
-    public bool fireratePowerUpActivated = false;
+    public float fireratePowerUpDuration = 20f;
+    public bool fireratePowerUpActivated;
 
-    private void Start()
-    {
-        fireratePowerUpActivated = false;
-    }
+
 
     private void OnCollisionStay2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            fireratePowerUpActivated = true;
             Destroy(gameObject);
             DoublePlayersFireRate(collision.gameObject);
         }
     }
-
+    
     public void DoublePlayersFireRate(GameObject player)
     {
         WeaponAttributes weaponAttributes = player.GetComponent<WeaponAttributes>();
-        weaponAttributes.UpdateShootDelay(doublefirerateduration);
+        weaponAttributes.UpdateShootDelay(fireratePowerUpDuration);
     }
 }
